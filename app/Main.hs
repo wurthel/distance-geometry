e37eb5540d03f9aece33f0bccfe8f289ba4915fb
Vusal Salmanov <salmanov.vh@gmail.com>
2019-05-15 19:40:57 +0300

    Update

--- app/Main.hs
+++ app/Main.hs
@@ -5,25 +5,18 @@ import DistanceGeometry
 import IO
 import Numeric.LinearAlgebra
 import Numeric.LinearAlgebra.Data
+import System.Directory
+
+idir = "test/molecules"
+odir = "test/results"
 
 main :: IO ()
 main = do
-  let (atoms, bonds) = readMolV2000 "example/methan.mol"
-  let s0@(u, l) =
-        (triangleInequalitySmoothingFloyd . generateDistanceBoundsMatrix atoms) bonds
-  s1 <- randomDistanceMatrix s0
-  let s2 =
-        (generateCoordinFromEigValAndVec .
-         largestEigValAndVec . distanceMatrixToMetricMatrix)
-          s1
-  let dm = coordMatrixToDistanceMatrix s2
-      errf1 = distanceErrorFunction1 dm u l
-      errf2 = distanceErrorFunction2 dm u l
-      errf3 = distanceErrorFunction3 dm u l
-  let newmole = updateCoordinates s2 atoms
-  writeXYZ "test.xyz" "Comment" newmole
-  --writeMoleculeXYZ "test.xyz" "Comment Line" atoms
-  print newmole
-  print errf1
-  print errf2
-  print errf3
+  putStrLn ""
+  putStrLn "generateOneMolecule: Begin"
+  --generateOneMolecule idir odir "example"
+  putStrLn "generateOneMolecule: End"
+  putStrLn ""
+  putStrLn "generateManyMolecules: Begin"
+  --generateManyMolecules idir odir "C2Br4" 0.01 100
+  putStrLn "generateManyMolecules: End"
